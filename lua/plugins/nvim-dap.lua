302c48cb13b6244b08d93b77e1a14f41948acb2e
eric.zheng.dashun <eric.zheng.dashun@gmail.com>
2025-08-30 09:51:59 +0800

    Update dap for python.

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,66 +1,49 @@
 return {
-	"mfussenegger/nvim-dap",
-	config = function()
-		local dap = require("dap")
+    "mfussenegger/nvim-dap",
+    config = function()
+        local dap = require("dap")
 
-		-- python
-		local get_python_path = function()
-			local cwd = vim.fn.getcwd()
-			local separator = vim.fn.has("win32") == 1 and "\\" or "/"
+        -- python
+        local get_python_path = function()
+            local cwd = vim.fn.getcwd()
+            local separator = vim.fn.has("win32") == 1 and "\\" or "/"
 
-			local venv_path = cwd .. separator .. ".venv" .. separator
-			if vim.fn.has("win32") == 1 then
-				if vim.fn.executable(venv_path .. "Scripts" .. separator .. "python.exe") == 1 then
-					return venv_path .. "Scripts" .. separator .. "python.exe"
-				end
-			else
-				if vim.fn.executable(venv_path .. "bin" .. separator .. "python") == 1 then
-					return venv_path .. "bin" .. separator .. "python"
-				end
-			end
-		end
+            local venv_path = cwd .. separator .. ".venv" .. separator
+            if vim.fn.has("win32") == 1 then
+                if vim.fn.executable(venv_path .. "Scripts" .. separator .. "python.exe") == 1 then
+                    return venv_path .. "Scripts" .. separator .. "python.exe"
+                end
+            else
+                if vim.fn.executable(venv_path .. "bin" .. separator .. "python") == 1 then
+                    return venv_path .. "bin" .. separator .. "python"
+                end
+            end
+        end
 
-		dap.adapters.python = {
-			type = "executable",
-			command = get_python_path(),
-			args = { "-m", "debugpy.adapter" },
-		}
+        dap.adapters.python = {
+            type = "executable",
+            command = get_python_path(),
+            args = {"-m", "debugpy.adapter"},
+            options = {
+                source_filetype = "python"
+            }
+        }
 
-		dap.configurations.python = {
-			{
-				type = "python",
-				request = "launch",
-				name = "Launch file",
-				program = "${file}",
-				pythonPath = get_python_path,
-				stopOnEntry = true,
-			},
-		}
+        dap.configurations.python = {{
+            type = "python",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            pythonPath = get_python_path,
+            console = "integratedTerminal"
+        }}
 
-		-- c#
-		local netcoredbg_path
-		if vim.fn.has("win32") == 1 then
-			netcoredbg_path = vim.fn.expand("$MASON/packages/netcoredbg/netcoredbg/netcoredbg.exe")
-		else
-			netcoredbg_path = vim.fn.expand("$MASON/packages/netcoredbg/netcoredbg/netcoredbg")
-		end
-		dap.adapters.coreclr = {
-			type = "executable",
-			command = netcoredbg_path,
-			args = { "--interpreter=vscode" },
-		}
-
-		dap.configurations.cs = {
-			{
-				name = "Launch .NET Core project",
-				type = "coreclr",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
-				end,
-				cwd = "${workspaceFolder}",
-				stopOnEntry = false,
-			},
-		}
-	end,
+        -- c#
+        -- local netcoredbg_path
+        -- if vim.fn.has("win32") == 1 then
+        -- 	netcoredbg_path = vim.fn.expand("$MASON/packages/netcoredbg/netcoredbg/netcoredbg.exe")
+        -- else
+        -- 	netcoredbg_path = vim.fn.expand("$MASON/packages/netcoredbg/netcoredbg")
+        -- end
+    end
 }
