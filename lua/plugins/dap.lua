339ad81b90ef3a64630ba7066859e456897e3a6c
eric.zheng.dashun <eric.zheng.dashun@gmail.com>
2025-09-02 20:42:21 +0800

    Add debug tool for python.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -126,6 +126,43 @@ return {
         },
       }
       dap.configurations.c = dap.configurations.cpp
+
+      -- python
+      local get_python_path = function()
+        local cwd = vim.fn.getcwd()
+        local separator = vim.fn.has("win32") == 1 and "\\" or "/"
+
+        local venv_path = cwd .. separator .. ".venv" .. separator
+        if vim.fn.has("win32") == 1 then
+          if vim.fn.executable(venv_path .. "Scripts" .. separator .. "python.exe") == 1 then
+            return venv_path .. "Scripts" .. separator .. "python.exe"
+          end
+        else
+          if vim.fn.executable(venv_path .. "bin" .. separator .. "python") == 1 then
+            return venv_path .. "bin" .. separator .. "python"
+          end
+        end
+      end
+
+      dap.adapters.python = {
+        type = "executable",
+        command = get_python_path(),
+        args = { "-m", "debugpy.adapter" },
+        options = {
+          source_filetype = "python",
+        },
+      }
+
+      dap.configurations.python = {
+        {
+          type = "python",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          pythonPath = get_python_path,
+          console = "integratedTerminal",
+        },
+      }
     end,
   },
 }
