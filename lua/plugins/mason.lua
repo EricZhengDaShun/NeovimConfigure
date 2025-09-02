047d220e9930a3643eb22a95e64eeb3d57822ba2
eric.zheng.dashun <eric.zheng.dashun@gmail.com>
2025-09-02 18:02:56 +0800

    Add format function.

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,41 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+				},
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+					"clang-format",
+					"black",
+					"cmakelang",
+					"yamlfmt",
+					"jq",
+					"prettier",
+					"sql-formatter",
+				},
+				auto_update = false,
+				run_on_start = true,
+			})
+		end,
+	},
+}
