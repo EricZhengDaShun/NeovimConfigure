59a9b6aa4ad6206e922a42bdd6c5a01499038a4b
eric.zheng.dashun <eric.zheng.dashun@gmail.com>
2025-09-02 18:21:40 +0800

    Add cmake-tools module.

---
+++ lua/plugins/cmake-tools.lua
@@ -0,0 +1,29 @@
+return {
+	"Civitasv/cmake-tools.nvim",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+	},
+	ft = { "cmake", "cpp", "c" },
+	opts = {
+		cmake_soft_link_compile_commands = false,
+		cmake_compile_commands_options = {
+			action = "lsp",
+		},
+		cmake_command = "cmake",
+		cmake_build_directory = "build",
+		cmake_build_type = "Debug",
+		cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
+		cmake_regenerate_on_save = true,
+		cmake_dap_configuration = { "cppdbg" },
+		cmake_executor = { name = "quickfix" },
+	},
+	keys = {
+		{ "<leader>cg", "<cmd>CMakeGenerate<CR>", desc = "CMake Generate" },
+		{ "<leader>cb", "<cmd>CMakeBuild<CR>", desc = "CMake Build" },
+		{ "<leader>cq", "<cmd>CMakeQuickBuild<CR>", desc = "CMake Quick Build" },
+		{ "<leader>cr", "<cmd>CMakeRun<CR>", desc = "CMake Run" },
+		{ "<leader>cd", "<cmd>CMakeDebug<CR>", desc = "CMake Debug" },
+		{ "<leader>cp", "<cmd>CMakeSelectConfigurePreset<CR>", desc = "CMake Select Configure Preset" },
+		{ "<leader>cP", "<cmd>CMakeSelectBuildPreset<CR>", desc = "CMake Select Build Preset" },
+	},
+}
