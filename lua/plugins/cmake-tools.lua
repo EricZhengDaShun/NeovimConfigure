af634520845f9215050273c8c8cd3f6e2d6e6641
eric.zheng.dashun <eric.zheng.dashun@gmail.com>
2025-09-08 01:43:31 +0800

    Update CMake-Tools for keymaps.

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -6,14 +6,16 @@ return {
     local function check()
       local cwd = vim.uv.cwd()
       if vim.fn.filereadable(cwd .. "/CMakeLists.txt") == 1 then
-        require("lazy").load { plugins = { "cmake-tools.nvim" } }
+        require("lazy").load({ plugins = { "cmake-tools.nvim" } })
         loaded = true
       end
     end
     check()
     vim.api.nvim_create_autocmd("DirChanged", {
       callback = function()
-        if not loaded then check() end
+        if not loaded then
+          check()
+        end
       end,
     })
   end,
@@ -21,5 +23,13 @@ return {
     cmake_compile_commands_options = {
       action = "lsp",
     },
+  },
+  keys = {
+    { "<leader>lcg", "<cmd>CMakeGenerate<cr>",          desc = "CMake Generate" },
+    { "<leader>lcb", "<cmd>CMakeBuild<cr>",             desc = "CMake Build" },
+    { "<leader>lcr", "<cmd>CMakeRun<cr>",               desc = "CMake Run" },
+    { "<leader>lcc", "<cmd>CMakeClean<cr>",             desc = "CMake Clean" },
+    { "<leader>lct", "<cmd>CMakeSelectBuildTarget<cr>", desc = "CMake Select Target" },
+    { "<leader>lcd", "<cmd>CMakeDebug<cr>",             desc = "CMake Debug" },
   },
 }
